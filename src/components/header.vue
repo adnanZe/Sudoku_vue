de6d3f6b57af2f47ccf5b6e27c885a565eb481fc
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-12-07 13:40:12 +0200

    [feature] header component

---
+++ src/components/header.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+<template>
+  <header>
+    <img src="../assets/sudokuLogo.png" alt="logo-img" />
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: rgba(38, 38, 38, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem 0;
+}
+
+header > img {
+  height: 40px;
+}
+</style>
