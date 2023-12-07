9995a32986c85c7bec6acd6b8a796922854bab2f
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-12-07 14:13:41 +0200

    [feature] core component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
 import Header from "./components/header.vue";
 import Footer from "./components/footer.vue";
+import Core from "./components/core/core.vue";
 </script>
 
 <template>
   <Header></Header>
+  <Core></Core>
   <Footer></Footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+@media only screen and (min-width: 992px) {
+  main {
+    flex-direction: row;
+  }
+}
+</style>
