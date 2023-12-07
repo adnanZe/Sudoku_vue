0e370a74d9d74f177a89676ce37aa680812d2055
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-12-07 13:47:43 +0200

    [feature] footer component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from "./components/header.vue";
+import Footer from "./components/footer.vue";
 </script>
 
 <template>
   <Header></Header>
+  <Footer></Footer>
 </template>
 
 <style scoped></style>
---
+++ src/components/footer.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+<template>
+  <footer>
+    <a href="https://github.com/adnanZe/Sudoku_vue">
+      <i class="fab fa-github"></i>
+      see my code
+    </a>
+  </footer>
+</template>
+<style scoped>
+footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 1rem;
+  background-color: rgba(38, 38, 38, 0.6);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+i {
+  width: 2rem;
+  color: white;
+  font-size: 1.5rem;
+  margin-right: 0.5rem;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
